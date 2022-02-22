d5b75500167695a3a53a868b0af01409ab7f4819
Ustymscorpion <ustym.horin@gmail.com>
2022-02-22 21:09:22 +0200

    EDIT_FILE!

--- src/Calc.java
+++ src/Calc.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
 
+class Calc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int firstNum = sc.nextInt();
+        System.out.println("firstNum = " + firstNum);
+        System.out.println(firstNum);
+        /*double secondNum = sc.nextDouble();
+        System.out.println("second num = " + secondNum);*/
+    }
+}
